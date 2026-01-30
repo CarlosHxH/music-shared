2e9d7ee4182d04321776fda3d2634667e17a59f0
CarlosdHxH <carlosdaniel.prog@gmail.com>
2026-01-30 12:17:56 -0400

    Setup inicial Maven e Spring-boot

---
+++ backend/src/main/java/com/album/seplag/SeplagApplication.java
@@ -0,0 +1,13 @@
+package com.album.seplag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeplagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeplagApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/album/seplag/SeplagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.album.seplag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeplagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
