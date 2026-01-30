44b6834a0f1d84ebf9adac86eb19273c45d6e1d5
CarlosdHxH <carlosdaniel.prog@gmail.com>
2026-01-30 18:28:47 -0400

    Adiciona a classe PresignedUrlResponse para encapsular a resposta de URLs pré-assinadas.

---
+++ backend/src/main/java/com/album/seplag/dto/PresignedUrlResponse.java
@@ -0,0 +1,14 @@
+package com.album.seplag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PresignedUrlResponse {
+    private String url;
+    private Long expiresIn;
+}
+
