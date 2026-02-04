5ebaa047e860a69bf30e19c21c699e424a65b150
06817435145 <carlos.dias@facilmova.com.br>
2026-02-04 11:06:09 -0400

    Atualiza variáveis de ambiente no docker-compose.yml para usar valores padrão e adiciona configurações de servidores no Swagger para ambientes local e de produção.

--- backend/src/main/java/com/album/seplag/swagger/Swagger.java
+++ backend/src/main/java/com/album/seplag/swagger/Swagger.java
@@ -8,9 +8,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @OpenAPIDefinition
 @Configuration
@@ -22,12 +26,22 @@ public class Swagger {
         return new OpenAPI()
                 .components(new Components())
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Servidor Local"),
+                        new Server()
+                                .url("http://backend:8080")
+                                .description("Servidor de Produção")))
                 .info(new Info()
                         .title("Album API")
                         .version("1.0.0")
                         .description("API REST para gerenciamento de artistas e álbuns musicais")
                         .contact(new Contact()
                                 .name("Carlos")
-                                .email("carlosdaniel.prog@gmail.com")));
+                                .email("carlosdaniel.prog@gmail.com"))
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
     }
 }
